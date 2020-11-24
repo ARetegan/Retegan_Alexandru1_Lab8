61d4fe025217338c7c98888aa29dcc82b1b8a8cf
ALEXANDRU-GEORGE RETEGAN <alexandru.retegan@stud.ubbcluj.ro>
2020-11-24 19:31:04 +0200

    Add project files.

---
+++ Data/Retegan_Alexandru1_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Retegan_Alexandru1_Lab8.Models;
+
+namespace Retegan_Alexandru1_Lab8.Data
+{
+    public class Retegan_Alexandru1_Lab8Context : DbContext
+    {
+        public Retegan_Alexandru1_Lab8Context (DbContextOptions<Retegan_Alexandru1_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Retegan_Alexandru1_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Retegan_Alexandru1_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Retegan_Alexandru1_Lab8.Models.Category> Category { get; set; }
+    }
+}
---
+++ Migrations/20201124151336_Category.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Retegan_Alexandru1_Lab8.Migrations
+{
+    public partial class Category : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
